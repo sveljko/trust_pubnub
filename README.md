43d4cc8a7063c87ac20d5091c7d777f2781bcd03
Srdjan Veljkovic <sveljko@gvs.rs>
2016-02-19 23:53:57 +0100

    Added some basic error handling.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # trust_pubnub
 
 The Rust Pubnub client code - may evolve into a library, but, for now,
-it's just a few functions that do basic Pubnub stuff, with pretty much no
-error handling.
+it's just a few "low level" functions that do basic Pubnub stuff, with
+some basic error handling.
--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,113 @@
 extern crate hyper;
 extern crate serde_json;
 
-use hyper::Client;
+
 use std::io::Read;
 use serde_json::Value;
 
 
-fn pubnub_time() -> u64 {
-	let client = Client::new();
-	let mut res = client.get("http://pubsub.pubnub.com/time/0").send().unwrap();
-	let mut body = String::new();
-	res.read_to_string(&mut body).unwrap();
-	let json_response:Value = serde_json::from_str(&body[..]).unwrap();
-	return json_response.as_array().unwrap()[0].as_u64().unwrap();
+enum PubnubError {
+    HttpError,
+    IoError,
+    FormatError
 }
 
 
-fn pubnub_publish(pubkey: &str, keysub: &str, channel: &str, message: &str) -> bool {
-	let client = Client::new();
-	let url = "http://pubsub.pubnub.com/publish/".to_string() + pubkey + "/" + keysub + "/0/" + channel + "/0/" + message;
-	let mut res = client.get(&url).send().unwrap();
-	let mut body = String::new();
-	res.read_to_string(&mut body).unwrap();
-	let json_response:Value = serde_json::from_str(&body[..]).unwrap();
-	println!("publish response: {:?}", json_response);
-	return json_response.as_array().unwrap()[0].as_i64().unwrap() == 1;
+impl std::convert::From<hyper::error::Error> for PubnubError {
+    fn from(_: hyper::error::Error) -> Self {
+        PubnubError::HttpError
+    }
 }
 
 
-fn pubnub_subscribe(keysub: &str, channel: &str, timetoken: &str) -> (Vec<Value>, String) {
-	let client = Client::new();
-	let url = "http://pubsub.pubnub.com/subscribe/".to_string() + keysub + "/" + channel + "/0/" + timetoken;
-	let mut res = client.get(&url).send().unwrap();
-	let mut body = String::new();
-	res.read_to_string(&mut body).unwrap();
-	let json_response:Value = serde_json::from_str(&body[..]).unwrap();
-	println!("subscribe response: {:?}", json_response);
-	let mut result: Vec<Value> = Vec::new();
-	for x in json_response.as_array().unwrap()[0].as_array().unwrap() {
-		result.push(x.clone());
-	}
-	return (result,
-		json_response.as_array().unwrap()[1].as_string().unwrap().to_string());
+impl std::convert::From<std::io::Error> for PubnubError {
+    fn from(_: std::io::Error) -> Self {
+        PubnubError::IoError
+    }
+}
+
+
+impl std::convert::From<serde_json::error::Error> for PubnubError {
+    fn from(_: serde_json::error::Error) -> Self {
+        PubnubError::FormatError
+    }
+}
+
+
+impl std::fmt::Debug for PubnubError {
+    fn fmt(&self, f: &mut std::fmt::Formatter)-> std::fmt::Result {
+        let x = match *self { 
+            PubnubError::HttpError => "HTTP error", 
+            PubnubError::IoError => "I/O error", 
+            PubnubError::FormatError => "Format error", 
+        };
+        write!(f, "{}", x)
+    }
+}
+
+
+fn pubnub_time() -> Result<u64, PubnubError> {
+    let client = hyper::Client::new();
+    let mut res = try!(client.get("http://pubsub.pubnub.com/time/0").send());
+    let mut body = String::new();
+    try!(res.read_to_string(&mut body));
+    let json_response:Value = try!(serde_json::from_str(&body[..]));
+    let a = try!(json_response.as_array().ok_or(PubnubError::FormatError));
+    if a.len() != 1 { return Err(PubnubError::FormatError)};
+    a[0].as_u64().ok_or(PubnubError::FormatError)
+}
+
+
+fn pubnub_publish(pubkey: &str, keysub: &str, channel: &str, message: &str) -> Result<bool, PubnubError> {
+    let client = hyper::Client::new();
+    let url = "http://pubsub.pubnub.com/publish/".to_string() + pubkey + "/" + keysub + "/0/" + channel + "/0/" + message;
+    let mut res = try!(client.get(&url).send());
+    let mut body = String::new();
+    try!(res.read_to_string(&mut body));
+    let json_response:Value = try!(serde_json::from_str(&body[..]));
+    println!("publish response: {:?}", json_response);
+    let a = try!(json_response.as_array().ok_or(PubnubError::FormatError));
+    if a.len() != 3 { return Err(PubnubError::FormatError)};
+    match a[0].as_u64() {
+        Some(v) => Ok(v == 1),
+        None => Err(PubnubError::FormatError)
+    }
+}
+
+
+fn pubnub_subscribe(keysub: &str, channel: &str, timetoken: &str) -> Result<(Vec<Value>, String), PubnubError> {
+    let client = hyper::Client::new();
+    let url = "http://pubsub.pubnub.com/subscribe/".to_string() + keysub + "/" + channel + "/0/" + timetoken;
+    let mut res = try!(client.get(&url).send());
+    let mut body = String::new();
+    try!(res.read_to_string(&mut body));
+    let json_response:Value = try!(serde_json::from_str(&body[..]));
+    println!("subscribe response: {:?}", json_response);
+    let mut result: Vec<Value> = Vec::new();
+    let a = try!(json_response.as_array().ok_or(PubnubError::FormatError));
+    if a.len() < 2 { return Err(PubnubError::FormatError) };
+    for x in try!(a[0].as_array().ok_or(PubnubError::FormatError)) {
+	result.push(x.clone());
+    }
+    let token = try!(a[1].as_string().ok_or(PubnubError::FormatError));
+    Ok((result, token.to_string()))
 }
 
 
 fn main() {
-	println!("pubnub_time: {}", pubnub_time());
+    println!("pubnub_time: {:?}", pubnub_time());
+    
+    println!("pubnub_publish: {:?}", pubnub_publish("demo", "demo", "hello_world", "\"Hello world from Rust!\""));
+    
+    let (msg, token) = match pubnub_subscribe("demo", "hello_world", "0") {
+        Ok(x) => x,
+        Err(_) => (Vec::<Value>::new(), "lalal".to_string())
+    };
+    println!("pubnub_subscribe: msg={:?}, token={}", msg, token);
 
-	println!("pubnub_publish: {}", pubnub_publish("demo", "demo", "hello_world", "\"Hello world from Rust!\""));
-
-	let (msg, token) = pubnub_subscribe("demo", "hello_world", "0");
-	println!("publish_subscribe: msg={:?}, token={}", msg, token);
-	let (msg, token) = pubnub_subscribe("demo", "hello_world", &token);
-	println!("publish_subscribe: msg={:?}, token={}", msg, token);
+    let (msg, token) = match pubnub_subscribe("demo", "hello_world", &token) {
+        Ok(x) => x,
+        Err(_) => (Vec::<Value>::new(), "lalal".to_string())
+    };
+    println!("pubnub_subscribe: msg={:?}, token={}", msg, token);
 }
